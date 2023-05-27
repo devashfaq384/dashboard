bb924e2e611924a2ff6cf75eb4c32966da1439b4
devashfaq384 <98392313+devashfaq384@users.noreply.github.com>
2023-05-27 10:08:01 +0500

    Update Ytd.tsx

--- Ytd.tsx
+++ Ytd.tsx
@@ -1,16 +1,1 @@
-import React from 'react'
 
-const Ytd = () => {
-  return (
-      <>
-        <div className='bg-white w-full mx-3  rounded-lg h-full py-5 px-2 text-center ' >
-            <div className='font-bold text-[25px] py-3' >Current Ratio - YTD</div>
-            <div className=' flex justify-center items-center gap-4 font-bold text-[30px]' >   
-                932
-            <img src="/mask-group.svg" alt="" />
-            </div>
-        </div>
-      </>
-  )
-}
-export default Ytd
